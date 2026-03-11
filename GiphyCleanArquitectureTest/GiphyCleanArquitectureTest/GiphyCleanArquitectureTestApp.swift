1896949a2d099c6c828e055838dac6e1d471206a
Carlos Jimenez <cjimenez91.dev@gmail.com>
2026-03-11 11:37:54 +0100

    Proyecto nuevo

---
+++ GiphyCleanArquitectureTest/GiphyCleanArquitectureTest/GiphyCleanArquitectureTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiphyCleanArquitectureTestApp.swift
+//  GiphyCleanArquitectureTest
+//
+//  Created by Carlos Jimenez Quinto on 11/3/26.
+//
+
+import SwiftUI
+
+@main
+struct GiphyCleanArquitectureTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
